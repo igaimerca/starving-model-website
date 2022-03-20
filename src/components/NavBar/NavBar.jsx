fa2130824fee262e2912e03564ca612c1fa69ba1
igaimerca <igaimerca@gmail.com>
2022-03-20 10:25:52 +0200

    feat: header responsiveness

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./NavBar.css";
+import useWindowDimensions from "../../utils/useWindowDimensions";
 function NavBar() {
+    const menuIcon = useRef(null);
+    const [showNavBar, setShowNavBar] = useState(null);
+    let { width } = useWindowDimensions();
+    console.log(width);
+
     return (
-        <nav className="navbar">
+        <nav className={`${width <= 837 && showNavBar ? "mobile-navbar" : ""} navbar`}>
             <h1>Starving <br /> Models</h1>
-            <div className="nav-items">
-                <ul className="nav-links">
+            <div className={`${width <= 837 && showNavBar ? "mobile-nav-items" : ""} nav-items`}>
+                <ul className={`${width <= 837 ? "mobile-nav-links" : ""} nav-links`}>
                     <li>Project</li>
                     <li>Art</li>
                     <li>Charities</li>
@@ -19,6 +25,11 @@ function NavBar() {
                     <li><img src="/images/social/instagram.svg" alt="" /></li>
                     <li><img src="/images/social/angrybearz.svg" alt="" /></li>
                 </ul>
+            </div>
+            <div className="menu-icon" ref={menuIcon} onClick={() => setShowNavBar(!showNavBar)}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h7" />
+                </svg>
             </div>
 
         </nav>
